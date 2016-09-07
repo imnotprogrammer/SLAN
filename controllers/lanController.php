bffa3b80b69ee8319dfd9195c487224cd8488000
joh <1441360095@qq.com>
2016-09-07 14:08:50 +0800

    Update lanController.php

--- controllers/lanController.php
+++ controllers/lanController.php
@@ -6,15 +6,9 @@ use frame\Controller;
  * 测试实例
  */
 class lanController extends Controller{
-    public function indexAction()
-    {
+    public function indexAction(){
         $this->assign('title', '欢迎你使用SLan框架');
         $this->display('index.php');
-
-        $db = new \frame\db\DBActive();
-        //$db->table('tb_user')->save(["test_name"=>"lanyuguo","test_pass"=>"1234567890"]);
-        $db->table('tb_user')->where(array("test_name"=>"lan","test_id"=>24))->update(["test_pass"=>2222232]);
-
     }
 }
 ?>
