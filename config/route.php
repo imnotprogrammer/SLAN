dea0c9593caa8b2a97b5a7e064b61bc589454232
joh <1441360095@qq.com>
2016-09-07 14:19:31 +0800

    Update route.php

--- config/route.php
+++ config/route.php
@@ -1,8 +1,12 @@
 <?php
+/**
+ * 路由配置信息
+ * */
+ 
     return array(
-        'defaultController' => 'lan',
-        'defaultAction' => 'index',
-        'defaultroute'   =>  'r'   //获取路由的缺省方式，r代表利用r=controller/action,/代表 /controller/action
+        'defaultController' => 'lan',       //缺省访问的控制器名称
+        'defaultAction' => 'index',         //缺省的访问方法名称
+        'defaultroute'   =>  'r'            //获取路由的缺省方式，r代表利用r=controller/action,/代表 /controller/action
     );
 
 ?>
