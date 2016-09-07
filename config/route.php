0458db12693ebdf91ebdfd03962339158718457d
joh <1441360095@qq.com>
2016-09-07 14:21:39 +0800

    Update route.php

--- config/route.php
+++ config/route.php
@@ -6,7 +6,8 @@
     return array(
         'defaultController' => 'lan',       //缺省访问的控制器名称
         'defaultAction' => 'index',         //缺省的访问方法名称
-        'defaultroute'   =>  'r'            //获取路由的缺省方式，r代表利用r=controller/action,/代表 /controller/action
+        'defaultroute'   =>  'r'            /*获取路由的缺省方式，此处可以设置为r和/两种值；
+                                            'r'代表利用r=controller/action,'/'代表 /controller/action*/
     );
 
 ?>
