c5ccdf5a14f554676e19587e629100c1147a11ea
joh <1441360095@qq.com>
2016-09-07 14:14:23 +0800

    Update email.php

--- config/email.php
+++ config/email.php
@@ -1,11 +1,11 @@
 <?php
     return array(
-        'EmailServer'     =>   'smtp.163.com',
-        'FromUserAddr'    =>   'youyilunmingyue@163.com',
-        'Password'        =>   '199312wasd',
-        'FromUserName'    =>   '清风送月',
-        'SMTPSecure'      =>   'ssl',
-        'Port'            =>   '465',
-        'Charset'         =>   'utf8'
+        'EmailServer'     =>   '',                                     //邮件服务器 例如smtp.163.com
+        'FromUserAddr'    =>   '',                                     //发件人的邮箱地址，例如：lanshiqingfeng@163.com
+        'Password'        =>   '',                                     //密码
+        'FromUserName'    =>   '',                                     //发件人的名称 例如 蓝雨果
+        'SMTPSecure'      =>   'ssl',                                  //采用的发送协议，默认为ssl
+        'Port'            =>   '465',                                  //邮件服务器端口 默认为465
+        'Charset'         =>   'utf8'                                  //邮件内容的编码形式，默认为utf8
     );
 ?>,
