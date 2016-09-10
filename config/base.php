2c0aa843694f651e1ffae479a7c7f1679b1f135e
imnotprogrammer <1441360095@qq.com>
2016-09-10 16:10:03 +0800

    SLAN Frame update

--- config/base.php
+++ config/base.php
@@ -1,6 +1,7 @@
 <?php
     return array(
         'web_host' => 'localhost',
+		'smartyEngine' => 'on',    //smarty模板引擎是否启动，on表示启动，off表示关闭，默认为on开启
         'extension' => array(
             'PHPMailer' => array(   //扩展名，即目录名
                 'loadfile' => array('PHPMailerAutoload.php')//需要加载的文件
@@ -8,7 +9,7 @@
             'PHPExcel' => array(
                 'loadfile' => array('PHPExcel/Shared/String.php','PHPExcel.php','PHPExcel/Writer/Excel2007.php')
             )
-        ),
+        ),	
     );
 
 ?>
