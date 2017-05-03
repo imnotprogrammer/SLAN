93656704b13834342dc4f41b306405a92db66185
imnotprogrammer <1441360095@qq.com>
2017-05-03 16:47:08 +0800

    d

---
+++ config/define.php
@@ -0,0 +1,7 @@
+<?php
+   define('NETMOON_APPLY_ID',1);
+   define('NETMOON_AUTOLOGIN_TIME',10*24*3600);
+   define('NETMOON_COMMON_API_TOKEN','d6cf64c676c48f3ea29660a7d829fe81');
+   define('NETMOON_COMMON_DOMAIN','http://logapi.9451.com');
+   
+?>
---
+++ config/global_arr.php
@@ -0,0 +1,43 @@
+<?php 
+    return array(
+    		'soiltype'=>array(
+    			0 => '普通土地',
+    			1 => '红土地',
+    			2 => '黑土地',
+    			3 => '金土地',
+    			4 => '紫土地',
+    		),
+    		'soiljieudan'=>array(
+    			1 => 'si_jieduanone',
+    			2 => 'si_jieduantwo',
+    			3 => 'si_jieduanthree',
+    			4 => 'si_jieduanfour',
+    			5 => 'si_jieduanfive'	
+    		),
+    		//增产
+    		'soiladd'=>array(
+    		    0 => 0,
+    			1 => 10,
+    			2 => 20,
+    			3 => 28,
+    			4 => 30
+    		),
+    		//成熟时间
+    		'soilshu'=>array(
+    			0 => 0,
+    			1 => 0,
+    			2 => 20,
+    			3 => 20,
+    			4 => 25,  				
+    		),
+    		//经验值增加
+    		'soilexpr'=>array(
+    			0 => 0,
+    			1 => 0,
+    			2 => 0,
+    			3 => 28,
+    			4 => 30
+    		)
+    		
+    );
+?>
