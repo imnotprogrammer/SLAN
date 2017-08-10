884b300c423242902a72962d43f04dbe4bf42747
joh <1441360095@qq.com>
2017-08-10 11:34:07 +0800

    Update base.php

--- config/base.php
+++ config/base.php
@@ -6,19 +6,19 @@
         'web_host' => 'localhost',
         'timezone' => 'GMT',
 		//'ifsessionshare' =>array(''),
-		'smarty' => array(
-			    'class'=>'spread\smarty\libs\Basesmarty',
-				'status' => true, //smarty模板引擎是否启动，true表示启动，false表示关闭，默认为on开启			
-				'template_dir' => WEB_ROOT.'/views',
-				'compile_dir' => WEB_ROOT.'/spread/smarty/template_c',
-				'plugins_dir' => WEB_ROOT.'/spread/smarty/plugins',
-				'cache_dir' => WEB_ROOT.'/spread/smarty/cache',
-				'config_dir' => WEB_ROOT.'/spread/smarty/config',
-				'caching' => false,
-				'cache_lifetime'=> 60*60*24,
-				'left_delimiter'=>'{',
-				'right_delimiter'=>'}'
-		    ),
+	'smarty' => array(
+	         'class'=>'spread\smarty\libs\Basesmarty',
+		 'status' => true, //smarty模板引擎是否启动，true表示启动，false表示关闭，默认为on开启			
+		 'template_dir' => WEB_ROOT.'/views',
+		 'compile_dir' => WEB_ROOT.'/spread/smarty/template_c',
+		  'plugins_dir' => WEB_ROOT.'/spread/smarty/plugins',
+		  'cache_dir' => WEB_ROOT.'/spread/smarty/cache',
+		 'config_dir' => WEB_ROOT.'/spread/smarty/config',
+		 'caching' => false,
+		 'cache_lifetime'=> 60*60*24,
+		'left_delimiter'=>'{',
+		'right_delimiter'=>'}'
+         ),
         'route'=>$route,
         'email'=>$email,
         'db'=>$db,		
