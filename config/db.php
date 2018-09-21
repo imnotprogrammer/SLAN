86222932bd512b74da023e2e1723dea18ad5a8aa
joh <1441360095@qq.com>
2018-09-21 10:11:32 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,10 +5,10 @@
 
    return array(
        "class"=>'spread\db\Db',
-       "db_host" => '192.168.15.30',          //数据库所在的地址；
-       "db_name" => 'test',                  //数据库的名称
-       "db_user" => 'root' ,                  //数据库用户名
-       "db_pass" => 'wasd199312',                   //数据库密码
+       "db_host" => 'localhost',          //数据库所在的地址；
+       "db_name" => 'database',                  //数据库的名称
+       "db_user" => 'yourusername' ,                  //数据库用户名
+       "db_pass" => 'yourpassword',                   //数据库密码
        "db_charset" => 'utf8'             //编码类型，默认为utf8
    );
 
